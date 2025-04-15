521e4c7f4eb936d2edd7f91fa137ed045a5beb0c
kokom <ka.mahrous@gmail.com>
2025-04-15 16:34:02 +0200

    DiscountCalculatorTest commit

--- JFree/src/Main.java
+++ JFree/src/Main.java
@@ -1,9 +1,16 @@
+import JFree.DiscountCalculator;
+import org.jfree.data.time.Week;
+
+import java.util.Calendar;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        Calendar calendar=Calendar.getInstance();
+        calendar.set(2025,Calendar.JANUARY,1);
+        Date date=calendar.getTime();
+        Week week=new Week(date);
+        DiscountCalculator discountCalculator=new DiscountCalculator(week);
+        System.out.println(week.getWeek());
     }
 }
--- JFree/src/test/DiscountCalculatorTest.java
+++ JFree/src/test/DiscountCalculatorTest.java
@@ -5,23 +5,18 @@ import org.jfree.data.time.Week;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-
 public class DiscountCalculatorTest {
     DiscountCalculator discountCalculator;
+    Calendar calendar=Calendar.getInstance();
 
     public void setup() throws Exception {
         DiscountCalculator discountCalculator = new DiscountCalculator(new Week());
     }
     @Test
     public void testIsTheSpecialWeekWhenFalse() throws Exception {
-        Calendar calendar = Calendar.getInstance();
         calendar.set(2025, Calendar.MARCH, 22);  // March 22, 2025
         Date date = calendar.getTime();
         Week week = new Week(date);
@@ -31,11 +26,28 @@ public class DiscountCalculatorTest {
 
     @Test
     public void testIsTheSpecialWeekWhenTrue() throws Exception {
-        Calendar calendar = Calendar.getInstance();
         calendar.set(2025, Calendar.JUNE, 23);  // March 29, 2025
         Date date = calendar.getTime();
         Week week = new Week(date);
         discountCalculator = new DiscountCalculator(week);
         Assert.assertTrue(discountCalculator.isTheSpecialWeek());
     }
+
+    @Test
+    public void testDiscountPercentWhenWeekIsOdd() throws Exception{
+        calendar.set(2025,Calendar.JANUARY,1);
+        Date date=calendar.getTime();
+        Week week=new Week(date);
+        discountCalculator=new DiscountCalculator(week);
+        Assert.assertEquals(discountCalculator.getDiscountPercentage(),5);
+    }
+    @Test
+    public void testDiscountPercentWhenWeekIsEven() throws Exception{
+        calendar.set(2025,Calendar.JANUARY,9);
+        Date date=calendar.getTime();
+        Week week=new Week(date);
+        discountCalculator=new DiscountCalculator(week);
+        Assert.assertEquals(discountCalculator.getDiscountPercentage(),7);
+    }
 }
+
