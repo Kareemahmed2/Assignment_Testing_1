76c7c264e850138d85aaba9fe741f508c51bbee0
kokom <ka.mahrous@gmail.com>
2025-04-16 12:16:31 +0200

    Discount manager mock testing commit

--- JFree/src/test/DiscountManagerTest.java
+++ JFree/src/test/DiscountManagerTest.java
@@ -4,10 +4,12 @@ import JFree.IDiscountCalculator;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DiscountManagerTest {
-
+    Mockery mockingContext=new Mockery();
+    IDiscountCalculator mockedCalculator = mockingContext.mock(IDiscountCalculator.class);
     @Test
     public void testCalculatePriceWhenDiscountsSeasonIsFalse() throws Exception {
         // Arrange
@@ -19,22 +21,58 @@ public class DiscountManagerTest {
         IDiscountCalculator mockedDependency = mockingContext.mock(IDiscountCalculator.class);
         mockingContext.checking(new Expectations(){
             {
-                // make sure that none of the functions are called
+                never(mockedDependency).getDiscountPercentage();
+                never(mockedDependency).isTheSpecialWeek();
             }
         });
         DiscountManager discountManager = new DiscountManager(isDiscountsSeason, mockedDependency);
         // Act
-
-
-
+        double actualPrice=discountManager.calculatePriceAfterDiscount(originalPrice);
         // Assert
+        assertEquals(expectedPrice,actualPrice,0.001);
         // make sure that mocking Expectations Is Satisfied
-        // make sure that the actual value exactly equals the expected value
+        mockingContext.assertIsSatisfied();
+    }
+
+    @Test
+    public void testCalculatePriceWhenDiscountsSeasonIsTrueAndSpecialWeekIsFalse()throws Exception{
+        boolean isDiscountSeason=true;
+        double originalPrice=150.0;
+        int mockPercentage=5;
+        double expectedPrice=150*(1-mockPercentage/100.0);
+        mockingContext.checking(new Expectations(){
+            {
+                oneOf(mockedCalculator).isTheSpecialWeek();
+                will(returnValue(false));
+                oneOf(mockedCalculator).getDiscountPercentage();
+                will(returnValue(mockPercentage));
+            }
+        });
+        DiscountManager discountManager=new DiscountManager(isDiscountSeason,mockedCalculator);
+        double actualPrice=discountManager.calculatePriceAfterDiscount(originalPrice);
+        assertEquals(expectedPrice,actualPrice,0.001);
+        mockingContext.assertIsSatisfied();
     }
 
     @Test
     public void testCalculatePriceWhenDiscountsSeasonIsTrueAndSpecialWeekIsTrue() throws Exception {
-
+        // Arrange
+        boolean isDiscountsSeason = true;
+        double originalPrice = 100.0;
+        double expectedPrice = 100.0*0.8;
+        mockingContext.checking(new Expectations(){
+            {
+                oneOf(mockedCalculator).isTheSpecialWeek();
+                will(returnValue(true));
+                never(mockedCalculator).getDiscountPercentage();
+            }
+        });
+        DiscountManager discountManager = new DiscountManager(isDiscountsSeason, mockedCalculator);
+        // Act
+        double actualPrice=discountManager.calculatePriceAfterDiscount(originalPrice);
+        // Assert
+        assertEquals(expectedPrice,actualPrice,0.001);
+        // make sure that mocking Expectations Is Satisfied
+        mockingContext.assertIsSatisfied();
     }
-
 }
